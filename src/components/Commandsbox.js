cce5ac39cbd268a5f425634f12ad4da05a5a44a1
Valentina Pezzi <valente.pezzi@gmail.com>
2022-04-18 14:00:57 +0200

    fixed tweet button

--- src/components/Commandsbox.js
+++ src/components/Commandsbox.js
@@ -1,14 +1,15 @@
 import React from "react"
 
 export default function Commandsbox(props) {
+    let currentQuote = props.quotetxt + ' -' + props.authortxt
     return(
         <div id='commands'>
             <div id='tweet'>
                 <img src={props.tw} alt=''></img>
                 <a id='tweet-quote' 
-                        class="twitter-share-button"
-                        href="https://twitter.com/intent/tweet"
-                        data-text={props.quotetxt + ' ' + props.authortxt}
+                        class='twitter-share-button'
+                        target="_blank"
+                        href={`https://twitter.com/intent/tweet/?text=${currentQuote}`}
                     >TWEET THIS</a>
             </div>
             <button id='new-quote' onClick={props.getQuote}>GET NEW QUOTE</button>
