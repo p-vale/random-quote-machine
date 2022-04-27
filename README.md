973813095d946f0e60e0a9c28d727328ae43c3df
Valentina Pezzi <valente.pezzi@gmail.com>
2022-04-27 19:49:06 +0200

    standardization

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random quote machine
+
+This is a small project to try the React library.
+
+There is a file with quotes that are randomly displayed.
+Users can tweet the current quote and request a new one.
+
+The svg is a vectorialized early 17th century Tuscan frame decoration.
--- src/App.js
+++ src/App.js
@@ -4,34 +4,33 @@ import tw from './twitter-icon.png'
 import Quotebox from './components/Quotebox'
 import Commandsbox from './components/Commandsbox'
 
-
-
 export default function App() {
-    const [quoteObj, setQuote] = React.useState('')
+  const [quoteObj, setQuote] = React.useState('')
 
 
-    function getQuote() {
-        const arr = importedData
-        const randomNum = Math.floor(Math.random() * arr.length)
-        setQuote(arr[randomNum])
-    }
+  function getQuote() {
+    const arr = importedData
+    const randomNum = Math.floor(Math.random() * arr.length)
+    setQuote(arr[randomNum])
+  }
 
-    let Quote = quoteObj? quoteObj.quote : importedData[16].quote
-    let Author = quoteObj? quoteObj.author : importedData[16].author
+  let Quote = quoteObj ? quoteObj.quote : importedData[16].quote
+  let Author = quoteObj ? quoteObj.author : importedData[16].author
 
-    return (
-        <div id='container'>
-          <div id='box'>
-              <Quotebox 
-                authortxt={Author} 
-                quotetxt={Quote}
-                />
-              <Commandsbox 
-                tw={tw} authortxt={Author} 
-                quotetxt={Quote} 
-                getQuote={getQuote}
-                />
-          </div>
-        </div>
-    )
+  return (
+    <div id = 'container'>
+      <div id = 'box'>
+        <Quotebox 
+          authortxt = {Author} 
+          quotetxt = {Quote}
+          />
+        <Commandsbox 
+          tw = {tw}
+          authortxt = {Author} 
+          quotetxt = {Quote} 
+          getQuote = {getQuote}
+          />
+      </div>
+    </div>
+  )
 }
